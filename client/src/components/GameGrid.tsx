aa1465a6753df67e78d957ee5c4580f0115336ab
tomas <tomasbeyene21@gmail.com>
2023-06-25 15:23:14 +0300

    container

--- client/src/components/GameGrid.tsx
+++ client/src/components/GameGrid.tsx
@@ -1,20 +1,32 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, loading } = useGames();
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <>
       {error && <Text>{error}</Text>}
+
       <SimpleGrid
         padding={3}
         spacing={10}
-        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+        templateColumns="repeat(auto-fill, minmax(250px, 2fr))"
       >
+        {loading &&
+          skeleton.map(() => (
+            <GameCardContainer>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
--- client/src/hooks/useGames.ts
+++ client/src/hooks/useGames.ts
@@ -24,23 +24,27 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [loading,setLoading]=useState(false)
 
   useEffect(() => {
+    setLoading(true)
     const controller= new AbortController()
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal})
       .then((res) => {
         console.log("game data", res.data);
         setGames(res.data.results);
+        setLoading(false)
       })
       .catch((err) => {
           if(err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false)
       });
       return ()=> controller.abort()
   }, []);
 
-  return {games,error}
+  return {games,error,loading}
 
 }
 
