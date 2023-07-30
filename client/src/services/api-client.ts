69736ba4c8157e3a9ffb71c34480c92ca3ab8307
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2023-07-30 08:34:10 +0100

    Update api-client.ts

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -7,3 +7,13 @@ export default axios.create({
         key: '6a1865dd9f4e4e5cb0766d7cb453fe06'
     }
 })
+
+/*import axios from "axios"
+
+export default axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params:{
+
+        key: '6a1865dd9f4e4e5cb0766d7cb453fe06'
+    }
+})*/
